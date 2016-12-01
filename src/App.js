5390aa045ec6c91be64624ea0f22e64a56da3e4c
Zachary Taylor <zachtaylorstudios@gmail.com>
2016-11-30 19:27:54 -0800

    Can place Xs

--- src/App.js
+++ src/App.js
@@ -9,19 +9,30 @@ class App extends Component {
   }
 }
 
-class Square extends Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
   render() {
     const status = 'Next player: X';
